763abed043dd49d2d52a7aad915a4443c2381f65
Sir Washington <badgerthememelord@gmail.com>
2026-02-17 00:55:41 +0100

    WIP Air Source block

--- src/main/java/com/thepeeingboyairfryers/washwater/common/block/WWBlocks.java
+++ src/main/java/com/thepeeingboyairfryers/washwater/common/block/WWBlocks.java
@@ -1,6 +1,7 @@
 package com.thepeeingboyairfryers.washwater.common.block;
 
 import com.thepeeingboyairfryers.washwater.common.WashWater;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -14,6 +15,7 @@ public class WWBlocks {
 
     public static final DeferredBlock<CosmicMonoxideSpoutBlock> COSMIC_MONOXIDE_SPOUT = BLOCKS.registerBlock("cosmic_monoxide_spout", CosmicMonoxideSpoutBlock::new);
     public static final DeferredBlock<FathomlessFluidChasmBlock> FATHOMLESS_FLUID_CHASM = BLOCKS.registerBlock("fathomless_fluid_chasm", FathomlessFluidChasmBlock::new);
+    public static final DeferredBlock<WaterSourceAirBlock> WATER_SOURCE_AIR_BLOCK = BLOCKS.registerBlock("water_source_air_block", WaterSourceAirBlock::new, BlockBehaviour.Properties.of().replaceable().noCollission().noLootTable().air());
 
     public static void register(IEventBus bus) {
         BLOCKS.register(bus);
---
+++ src/main/java/com/thepeeingboyairfryers/washwater/common/block/WaterSourceAirBlock.java
@@ -0,0 +1,40 @@
+package com.thepeeingboyairfryers.washwater.common.block;
+
+import com.thepeeingboyairfryers.washwater.common.fluids.FluidUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.WeatheringCopper;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.neoforge.common.NeoForgeMod;
+
+public class WaterSourceAirBlock extends Block {
+
+
+    public WaterSourceAirBlock(Properties properties) {
+        super(properties);
+    }
+
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.INVISIBLE;
+    }
+
+    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        System.out.println("randomly ticked");
+        FluidUtil.addVolume(level, pos, NeoForgeMod.WATER_TYPE.value(), 1);
+    }
+
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.empty();
+    }
+
+    protected boolean isRandomlyTicking(BlockState state) {
+        return true;
+    }
+}
