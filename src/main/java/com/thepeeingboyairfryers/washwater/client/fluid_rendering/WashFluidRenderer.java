61472f5e1bc8ca3e119033ce317a9a8fcc5eebd4
Sir Washington <badgerthememelord@gmail.com>
2026-02-18 12:29:50 +0100

    Rendering Pt1

--- src/main/java/com/thepeeingboyairfryers/washwater/client/fluid_rendering/WashFluidRenderer.java
+++ src/main/java/com/thepeeingboyairfryers/washwater/client/fluid_rendering/WashFluidRenderer.java
@@ -1,5 +1,6 @@
 package com.thepeeingboyairfryers.washwater.client.fluid_rendering;
 
+import com.thepeeingboyairfryers.washwater.common.WaterInfo;
 import com.thepeeingboyairfryers.washwater.common.fluids.MultiFluidValue;
 import com.thepeeingboyairfryers.washwater.duck.ILevelSliceFluids;
 import net.caffeinemc.mods.sodium.api.util.NormI8;
@@ -16,14 +17,21 @@ import net.caffeinemc.mods.sodium.client.render.chunk.terrain.material.Material;
 import net.caffeinemc.mods.sodium.client.render.chunk.translucent_sorting.TranslucentGeometryCollector;
 import net.caffeinemc.mods.sodium.client.render.chunk.vertex.builder.ChunkMeshBufferBuilder;
 import net.caffeinemc.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
+import net.caffeinemc.mods.sodium.client.services.PlatformBlockAccess;
+import net.caffeinemc.mods.sodium.client.util.DirectionUtil;
 import net.caffeinemc.mods.sodium.client.world.LevelSlice;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class WashFluidRenderer extends FluidRenderer {
 
@@ -39,7 +47,7 @@ public class WashFluidRenderer extends FluidRenderer {
         this.lightPipelineProvider = iLightPipelineProvider;
         this.defaultFluidRenderer = new DefaultFluidRenderer(lightPipelineProvider);
     }
-
+    
     @Override
     public void render(
             LevelSlice level,
@@ -51,14 +59,103 @@ public class WashFluidRenderer extends FluidRenderer {
             ChunkBuildBuffers buffers
     ) {
         Material material = DefaultMaterials.forFluidState(fluidState);
-        ChunkModelBuilder meshBuilder = buffers.get(material);
+        ChunkModelBuilder builder = buffers.get(material);
         IClientFluidTypeExtensions handler = IClientFluidTypeExtensions.of(fluidState);
         ILevelSliceFluids fluids = (ILevelSliceFluids) (Object) level;
+
+        float generalHeight = ((float) fluids.ww€getFluidFor(blockPos.getX(), blockPos.getY(), blockPos.getZ()).getTotalVolume() / WaterInfo.VOLUME_PER_BLOCK);
+        float northWestHeight = generalHeight;
+        float northEastHeight = generalHeight;
+        float southWestHeight = generalHeight;
+        float southEastHeight = generalHeight;
+        float yOffset = 0.001F;
+
+        //Bottom Face
+        setVertex(0, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+        setVertex(1, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+        setVertex(2, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+        setVertex(3, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
 
         quad.setColor(0, 0xFFFFFFFF);
         quad.setColor(1, 0xFFFFFFFF);
         quad.setColor(2, 0xFFFFFFFF);
         quad.setColor(3, 0xFFFFFFFF);
+        writeQuad(builder, collector, material, offset, ModelQuadFacing.UNASSIGNED, false);
+
+        //Top Face
+        setVertex(0, 0.0f, southWestHeight, 1.0f, 0.0f, 0.0f);
+        setVertex(1, 1.0f, southEastHeight, 1.0f, 1.0f, 0.0f);
+        setVertex(2, 1.0f, northEastHeight, 0.0f, 1.0f, 1.0f);
+        setVertex(3, 0.0f, northWestHeight, 0.0f, 0.0f, 1.0f);
+
+        quad.setColor(0, 0xFFFFFFFF);
+        quad.setColor(1, 0xFFFFFFFF);
+        quad.setColor(2, 0xFFFFFFFF);
+        quad.setColor(3, 0xFFFFFFFF);
+        writeQuad(builder, collector, material, offset, ModelQuadFacing.UNASSIGNED, false);
+
+        for(Direction dir : DirectionUtil.HORIZONTAL_DIRECTIONS) {
+
+
+            float c1;
+            float c2;
+            float x1;
+            float z1;
+            float x2;
+            float z2;
+            switch (dir) {
+                case NORTH:
+                    c1 = northWestHeight;
+                    c2 = northEastHeight;
+                    x1 = 0.0F;
+                    x2 = 1.0F;
+                    z1 = 0.001F;
+                    z2 = z1;
+                    break;
+                case SOUTH:
+                    c1 = southEastHeight;
+                    c2 = southWestHeight;
+                    x1 = 1.0F;
+                    x2 = 0.0F;
+                    z1 = 0.999F;
+                    z2 = z1;
+                    break;
+                case WEST:
+                    c1 = southWestHeight;
+                    c2 = northWestHeight;
+                    x1 = 0.001F;
+                    x2 = x1;
+                    z1 = 1.0F;
+                    z2 = 0.0F;
+                    break;
+                case EAST:
+                    c1 = northEastHeight;
+                    c2 = northWestHeight;
+                    x1 = 0.999F;
+                    x2 = x1;
+                    z1 = 0.0F;
+                    z2 = 1.0F;
+                    break;
+                default:
+                    continue;
+            }
+
+            float u1 = 1f;
+            float u2 = 1f;
+            float u3 = 1f;
+            float v1 = 1f;
+            float v2 = 1f;
+            float v3 = 1f;
+
+
+            setVertex(0, x2, c2, z2, u2, v2);
+            setVertex(1, x2, yOffset, z2, u2, v3);
+            setVertex(2, x1, yOffset, z1, u1, v3);
+            setVertex(3, x1, c1, z1, u1, v1);
+            writeQuad(builder, collector, material, offset, ModelQuadFacing.UNASSIGNED, false);
+
+        }
+
 
         try {
             logger.info("Rendering block at {} with {}", blockPos, fluids.ww€getFluidFor(blockPos.getX(), blockPos.getY(), blockPos.getZ()).getTotalVolume());
