a333b368bf326fb6a3782fc5c5e15e0aada0406e
Sir Washington <badgerthememelord@gmail.com>
2026-03-12 00:28:38 +0100

    Cleanup

--- src/main/java/com/thepeeingboyairfryers/washwater/common/item/PrecisionBucketItem.java
+++ src/main/java/com/thepeeingboyairfryers/washwater/common/item/PrecisionBucketItem.java
@@ -4,9 +4,7 @@ import com.thepeeingboyairfryers.washwater.common.WaterInfo;
 import com.thepeeingboyairfryers.washwater.common.component.ModDataComponentTypes;
 import com.thepeeingboyairfryers.washwater.common.fluids.FluidUtil;
 import com.thepeeingboyairfryers.washwater.common.fluids.MultiFluidValue;
-import com.thepeeingboyairfryers.washwater.common.nbtUtil.DataComponentUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
@@ -16,19 +14,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
-import net.neoforged.neoforge.fluids.FluidType;
+import net.minecraft.world.phys.HitResult;
 
 import java.util.List;
 
 public class PrecisionBucketItem extends Item {
-
-    static int maxFillLevel = 1000;
 
     public PrecisionBucketItem(Item.Properties properties) {
         super(properties);
@@ -38,11 +30,8 @@ public class PrecisionBucketItem extends Item {
         Level level = useOnContext.getLevel();
         Player player = useOnContext.getPlayer();
         ItemStack itemStack = useOnContext.getItemInHand();
-        BlockPos targetPos = useOnContext.getClickedPos();
-
-        //DataComponentUtils.getOrCreateComponent(ModDataComponentTypes.BUCKET_FILL_LEVEL, itemStack);
-        //itemStack.set(ModDataComponentTypes.BUCKET_FILL_LEVEL, 0);
-
+        BlockHitResult blockHitResult = getPlayerPOVHitResult(level, player, net.minecraft.world.level.ClipContext.Fluid.NONE);
+        BlockPos targetPos = blockHitResult.getBlockPos().relative(blockHitResult.getDirection());
         if (player != null) {
             if (!player.isCrouching()) {
                 precisionBucketPlace(level, targetPos, itemStack, player);
@@ -83,80 +72,47 @@ public class PrecisionBucketItem extends Item {
         if (itemStack.get(ModDataComponentTypes.BUCKET_FILL_LEVEL) != null) {
             fillLevel = itemStack.get(ModDataComponentTypes.BUCKET_FILL_LEVEL);
         }
-        float fraction = (float) fillLevel / (float) maxFillLevel;
+        float fraction = (float) fillLevel / (float) WaterInfo.PRECISION_BUCKET_CAPACITY;
         return (int) (13f * fraction);
     }
 
-    public static boolean precisionBucketPlace(Level level, BlockPos pos, ItemStack itemStack, Player player) {
+    public static boolean precisionBucketPlace(Level level, BlockPos targetPos, ItemStack itemStack, Player player) {
         int fillLevel = 0;
         if (itemStack.get(ModDataComponentTypes.BUCKET_FILL_LEVEL) != null) {
             fillLevel = itemStack.get(ModDataComponentTypes.BUCKET_FILL_LEVEL);
         }
         int newBucketFillLevel = 0;
-
         if (fillLevel > 0 && !level.isClientSide) {
-            BlockHitResult blockHitResult = getPlayerPOVHitResult(level, player, net.minecraft.world.level.ClipContext.Fluid.NONE);
-            BlockPos blockPos = blockHitResult.getBlockPos();
-            Direction direction = blockHitResult.getDirection();
-            BlockPos blockPos2 = blockPos.relative(direction);
-            //NonCachedWater.addWater(bucketFillLevel, blockPos2, level);
-            FluidUtil.addVolume((ServerLevel) level, blockPos2, WaterInfo.WATER_TYPE, fillLevel);
+            FluidUtil.addVolume((ServerLevel) level, targetPos, WaterInfo.WATER_TYPE, fillLevel);
             itemStack.set(ModDataComponentTypes.BUCKET_FILL_LEVEL, newBucketFillLevel);
         }
         return true;
     }
 
-    protected static BlockHitResult getPlayerEntityPOVHitResult(Level level, Player player, ClipContext.Fluid fluid) {
-        float f = player.getXRot();
-        float g = player.getYRot();
-        Vec3 vec3 = player.getEyePosition();
-        float h = Mth.cos(-g * 0.017453292F - 3.1415927F);
-        float i = Mth.sin(-g * 0.017453292F - 3.1415927F);
-        float j = -Mth.cos(-f * 0.017453292F);
-        float k = Mth.sin(-f * 0.017453292F);
-        float l = i * j;
-        float n = h * j;
-        double d = 5.0;
-        Vec3 vec32 = vec3.add((double)l * 5.0, (double)k * 5.0, (double)n * 5.0);
-        return level.clip(new ClipContext(vec3, vec32, ClipContext.Block.OUTLINE, fluid, player));
-    }
-
-    public static boolean precisionBucketPickup(Level level, BlockPos pos, ItemStack itemStack, Player player) {
+    public static boolean precisionBucketPickup(Level level, BlockPos targetPos, ItemStack itemStack, Player player) {
         int fillLevel = 0;
         if (itemStack.get(ModDataComponentTypes.BUCKET_FILL_LEVEL) != null) {
             fillLevel = itemStack.get(ModDataComponentTypes.BUCKET_FILL_LEVEL);
         }
 
-        int bucketRemainingSpace = maxFillLevel - fillLevel;
+        int bucketRemainingSpace = WaterInfo.PRECISION_BUCKET_CAPACITY - fillLevel;
         if (!level.isClientSide) {
-            BlockHitResult blockHitResult = getPlayerPOVHitResult(level, player, net.minecraft.world.level.ClipContext.Fluid.NONE);
-            BlockPos blockPos = blockHitResult.getBlockPos();
-            Direction direction = blockHitResult.getDirection();
-            BlockPos blockPos2 = blockPos.relative(direction);
-            short oldVolume = FluidUtil.getVolume(level, blockPos2, WaterInfo.WATER_TYPE);
-
-            System.out.println("oldvol: " + oldVolume);
+            short oldVolume = FluidUtil.getVolume(level, targetPos, WaterInfo.WATER_TYPE);
             int newVolume = 0;
             int newBucketFillLevel;
             if (oldVolume > bucketRemainingSpace) {
                 newVolume = oldVolume - bucketRemainingSpace;
-                newBucketFillLevel = maxFillLevel;
+                newBucketFillLevel = WaterInfo.PRECISION_BUCKET_CAPACITY;
             }
             else {
                 newBucketFillLevel = fillLevel + oldVolume;
             }
             if (newVolume > 0) {
-                //level.setBlock(blockPos2, Fluids.WATER.getFlowing(newVolume, false).createLegacyBlock(), 11);
-                MultiFluidValue iValue = MultiFluidValue.single(WaterInfo.WATER_TYPE, (short) newVolume);
-                FluidUtil.setVolume((ServerLevel) level, blockPos2, iValue);
+                FluidUtil.setVolume((ServerLevel) level, targetPos, MultiFluidValue.single(WaterInfo.WATER_TYPE, (short) newVolume));
                 }
             else {
-                //level.setBlock(blockPos2, Blocks.AIR.defaultBlockState(), 11);
-                MultiFluidValue iValue = MultiFluidValue.single(WaterInfo.WATER_TYPE, (short) newVolume);
-                FluidUtil.setVolume((ServerLevel) level, blockPos2, iValue);
+                FluidUtil.setVolume((ServerLevel) level, targetPos, MultiFluidValue.single(WaterInfo.WATER_TYPE, (short) newVolume));
             }
-
-            System.out.println("set bucket to " + newBucketFillLevel);
             itemStack.set(ModDataComponentTypes.BUCKET_FILL_LEVEL, newBucketFillLevel);
         }
         return true;
