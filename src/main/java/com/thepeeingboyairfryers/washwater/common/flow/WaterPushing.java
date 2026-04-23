a2e9df4e16a922188fc0499ed20b50c9c3f56c1b
Sir Washington <badgerthememelord@gmail.com>
2026-04-23 16:55:21 +0200

    wip

--- src/main/java/com/thepeeingboyairfryers/washwater/common/flow/WaterPushing.java
+++ src/main/java/com/thepeeingboyairfryers/washwater/common/flow/WaterPushing.java
@@ -1,10 +1,14 @@
 package com.thepeeingboyairfryers.washwater.common.flow;
 
 import com.thepeeingboyairfryers.washwater.common.fluids.FluidUtil;
+import com.thepeeingboyairfryers.washwater.common.util.PseudoRandom;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.fluids.FluidType;
+
+import java.util.ArrayList;
 
 public class WaterPushing {
 
@@ -29,7 +33,7 @@ public class WaterPushing {
         while (volumeToDisplace > 0 && currentDistance < maxDistance) {
             newPos = newPos.relative(direction);
             currentDistance++;
-            volumeToDisplace = FluidUtil.addWaterVolumeAndReturnRemaining(level, newPos, type, volumeToDisplace);
+            volumeToDisplace = FluidUtil.addWaterVolumeAndReturnRemaining(level, newPos, type, volumeToDisplace, true);
         }
 
         return volumeToDisplace == 0;
@@ -56,4 +60,106 @@ public class WaterPushing {
 
         return volumeToDisplace == 0;
     }
+
+    //TODO Fix Ewoud not making addFluid return a bool (ability to fail)
+    public static boolean displaceFluids(ServerLevel level, BlockPos pos) {
+        FluidType type = level.getFluidState(pos).getFluidType();
+        int availableVolume = FluidUtil.getVolume(level, pos, type);
+        ArrayList<Direction> viableHorDirections = new ArrayList<>(0);
+        int maxLoops = 10;
+
+        boolean result = false;
+        for (Direction dir : PseudoRandom.getRandomDirectionArray()) {
+            if (!FluidUtil.isSolid(level, pos.relative(dir)) && !FluidUtil.isFilledUp(level, pos.relative(dir))) {
+                viableHorDirections.add(dir);
+            }
+        }
+
+        if (viableHorDirections.isEmpty()) {
+            if (!FluidUtil.isSolid(level, pos.above())) {
+                //TODO Here
+                //success = (FluidUtil.addVolume(level, pos, availableVolume);
+                FluidUtil.addVolume(level, pos, type, availableVolume);
+                return result;
+            }
+        }
+        else {
+            BlockPos.MutableBlockPos scratchPos = pos.mutable();
+            int i = 0;
+            while (availableVolume > 0 && i <= maxLoops) {
+                int cut = availableVolume / viableHorDirections.size();
+                for (Direction dir : viableHorDirections) {
+                    scratchPos = pos.relative(dir).mutable();
+                        //if (FluidUtil.addVolume(level, pos.relative(dir), type, stepSize))
+                        //availableVolume - stepSize
+                        int remainder = FluidUtil.addWaterVolumeAndReturnRemaining(level, scratchPos, type, cut, false);
+                        availableVolume = availableVolume - cut + remainder;
+                }
+                i++;
+            }
+        }
+        //System.out.println("available " + availableVolume);
+        result = availableVolume <= 4;
+        System.out.println("result: " + result);
+        return result;
+    }
+
+    //TODO Fix Ewoud not making addFluid return a bool (ability to fail)
+    public static boolean checkIfCanDisplaceFluids(ServerLevel level, BlockPos pos) {
+        FluidType type = level.getFluidState(pos).getFluidType();
+        int availableVolume = FluidUtil.getVolume(level, pos, type);
+        ArrayList<Direction> viableHorDirections = new ArrayList<>(0);
+        int maxLoops = 10;
+
+        boolean result = false;
+        for (Direction dir : PseudoRandom.getRandomDirectionArray()) {
+            if (!FluidUtil.isSolid(level, pos.relative(dir)) && !FluidUtil.isFilledUp(level, pos.relative(dir))) {
+                viableHorDirections.add(dir);
+            }
+        }
+
+        if (viableHorDirections.isEmpty()) {
+            if (!FluidUtil.isSolid(level, pos.above())) {
+                //TODO Here
+                //success = (FluidUtil.addVolume(level, pos, availableVolume);
+                FluidUtil.addVolume(level, pos, type, availableVolume);
+                return result;
+            }
+        }
+        else {
+            BlockPos.MutableBlockPos scratchPos = pos.mutable();
+            int i = 0;
+            while (availableVolume > 0 && i <= maxLoops) {
+                int cut = availableVolume / viableHorDirections.size();
+                for (Direction dir : viableHorDirections) {
+                    scratchPos = pos.relative(dir).mutable();
+                    //if (FluidUtil.addVolume(level, pos.relative(dir), type, stepSize))
+                    //availableVolume - stepSize
+                    int remainder = FluidUtil.addWaterVolumeAndReturnRemainingImaginary(level, scratchPos, type, cut, false);
+                    availableVolume = availableVolume - cut + remainder;
+                }
+                i++;
+            }
+        }
+        //System.out.println("available " + availableVolume);
+        result = availableVolume <= 4;
+        System.out.println("result: " + result);
+        return result;
+    }
+
+/*            else {
+        BlockPos.MutableBlockPos scratchPos = pos.mutable();
+        int cut = availableVolume / viableHorDirections.size();
+        while (availableVolume > 0) {
+            for (Direction dir : viableHorDirections) {
+                scratchPos.move(dir);
+                //if (FluidUtil.addVolume(level, pos.relative(dir), type, stepSize))
+                //availableVolume - stepSize
+                FluidUtil.addVolume(level, pos.relative(dir), type, cut);
+                availableVolume -= cut;
+
+
+            }
+        }
+    }*/
 }
---
+++ src/main/java/com/thepeeingboyairfryers/washwater/mixin/common/MixinBlockItem.java
@@ -0,0 +1,112 @@
+package com.thepeeingboyairfryers.washwater.mixin.common;
+
+
+import com.thepeeingboyairfryers.washwater.common.flow.WaterPushing;
+import com.thepeeingboyairfryers.washwater.common.fluids.FluidUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockItem.class)
+public class MixinBlockItem {
+
+
+
+/*    @Redirect(
+            method = "place",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/block/Block;setPlacedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V"
+            )
+    )
+    private void redirectBlockPlacement(Block instance, Level level, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+        System.out.println("Tu as mis un bloc");
+        if (FluidUtil.hasFluid(level, pos)) {
+            System.out.println("oui");
+            WaterPushing.displaceFluids((ServerLevel) level, pos);
+        }
+
+    }*/
+
+/*    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;setPlacedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V"), method = "place", cancellable = true)
+    private void redirectBlockPlacement(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        System.out.println("Tu as mis un bloc");
+        if (!level.isClientSide()) {
+            if (FluidUtil.getAllVolume(level, pos) != 0) {
+                System.out.println("oui");
+                if (WaterPushing.displaceFluids((ServerLevel) level, pos))
+                    cir.setReturnValue(InteractionResult.PASS);
+                else {
+                    cir.setReturnValue(InteractionResult.FAIL);
+                }
+            }
+        }
+
+    }*/
+/*    @Inject(at = @At(value = "HEAD"), method = "place", cancellable = true)
+    private void redirectBlockPlacement(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        System.out.println("Tu as mis un bloc");
+        if (!level.isClientSide()) {
+            if (FluidUtil.hasFluid(level, pos)) {
+                System.out.println("oui");
+                WaterPushing.displaceFluids((ServerLevel) level, pos);
+            }
+        }
+    }*/
+/*    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;setPlacedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V"), method = "place", cancellable = true)
+    private void pushWaterAway(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        System.out.println("Tu as mis un bloc");
+        if (!level.isClientSide()) {
+            System.out.println("Nous sommes dans le tick du server");
+            if (FluidUtil.hasFluid(level, pos)) {
+                System.out.println("Il y avait d'eau");
+                WaterPushing.displaceFluids((ServerLevel) level, pos);
+            }
+        }
+
+    }*/
+
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BlockItem;placeBlock(Lnet/minecraft/world/item/context/BlockPlaceContext;Lnet/minecraft/world/level/block/state/BlockState;)Z"), method = "place", cancellable = true)
+    private void pushWaterAway(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        System.out.println("Tu as mis un bloc");
+        if (!level.isClientSide()) {
+            if (FluidUtil.hasFluid(level, pos)) {
+                boolean success = true;
+                System.out.println("oui");
+                if (!WaterPushing.displaceFluids((ServerLevel) level, pos)) {
+
+                }
+
+
+            }
+        }
+        cir.setReturnValue(InteractionResult.FAIL);
+        cir.cancel();
+
+    }
+
+
+
+
+}
